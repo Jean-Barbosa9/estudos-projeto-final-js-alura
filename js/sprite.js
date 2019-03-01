4cdc9da7ebf5c91e6560483dbf9916fa05425fb2
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-03-01 08:13:02 -0300

    solucionando parte 2 proposta para o sprite

--- js/sprite.js
+++ js/sprite.js
@@ -1,22 +1,3 @@
-// Entendo que essa seria uma forma de começar a resolver o problema, mas no briefing ele exige que createSprite seja uma função. Aqui eu teria que inicializar a classe como createSprite, para que ele possa chamar no index da forma como ele colocou, mas não é a sintaxe mais correta
-// class Sprite {
-//   constructor(jqSelector) {
-//     this.createSprite(jqSelector)
-//   }
-
-//   createSprite(jqSelector) {
-//     return $(jqSelector)
-//   }
-
-//   nextFrame() {
-
-//   }
-// }
-
-// const createSprite = new Sprite()
-
-// Outra forma de fazer seria criando uma função que retorna um objeto, mas tô em dúvida de como isso poderia retornar tanto o objeto com o método nextFrame quanto a seleção do elemento HTML, passando pelo parâmetro
-
 function createSprite(jqSelector) {
   return sprite = {
     selectedElement: $(jqSelector),
@@ -24,17 +5,28 @@ function createSprite(jqSelector) {
 
     nextFrame: function(){
       const { selectedElement } = sprite
-      let frameDigit = sprite.frameDigit,
       regex = /frame(\d)/,
       classList = selectedElement.attr('class').split(/\s+/)
 
       $(classList).each(function(){
         if(regex.test(this)) {
-          frameDigit = parseInt(this.match(/frame(\d)/)[1])
-          frameDigit++
+          sprite.frameDigit = parseInt(this.match(/frame(\d)/)[1])
         }
       })
-      selectedElement.removeClass(String('frame' + frameDigit-1)).addClass('frame' + frameDigit)
+      sprite.frameDigit < 9 ? selectedElement.removeClass(String('frame' + sprite.frameDigit)).addClass('frame' + ++sprite.frameDigit) : ''
+    },
+
+    reset: function() {
+      var classList = sprite.selectedElement.attr('class').split('/\s+/')
+      var regex = /frame\d/
+      $(classList).each(function(){
+        var thisClass = this.match(regex)[0]
+        if(regex.test(this)) sprite.selectedElement.removeClass(thisClass)
+      })
+    },
+
+    isFinished: function() {
+      return sprite.frameDigit == 9
     }
   }
 }
