96c728839e73f74bbff268e878755e23e2fbe6f0
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-03-05 14:55:11 -0300

    aula 3 atividade 4

--- js/jogo.js
+++ js/jogo.js
@@ -1,27 +1,56 @@
-var criaJogo = function() {
-  var palavraSecreta = '';
-  var lacunas = [];
-  var etapa = 1
+var game = function(sprite) {
+  var secret = '';
+  var gap = [];
+  var step = 1;
+  var inputs = []; //TODO: Colocar uma validação para verificar se o usuário já chutou essa opção antes, para não correr o risco de ser punido por estar chutando a mesma opção novamente.
 
-  var setPalavraSecreta = function(palavra) {
-    palavraSecreta = palavra
-    etapa++
-    return palavraSecreta
+  var setSecret = function(word) {
+    secret = word
+    gap = Array(secret.length).fill('')
+    step++
   };
 
-  var getEtapa = function() {
-    return etapa
+  var correctInput = function() {
+    // depois vai printar as letras na tela
+    console.log(gap)
   };
 
-  var getLacunas = function() {
-    return lacunas = palavraSecreta.split(/\w/).slice(0,palavraSecreta.length)
+  var wrongInput = function() {
+    sprite.nextFrame()
   };
 
+  var processInput = function(input) {
+    var errors = 0;
+    for(var i=0, len=secret.length;i<len;i++) {
+      if(secret[i] == input) {
+        gap[i] = input;
+      }
+      else {
+        errors++;
+      }
+    };
 
+
+
+    if(errors == secret.length) {
+      wrongInput()
+    } else {
+      correctInput()
+    };
+  };
+
+  var getGap = function() {
+    return gap
+  };
+
+  var getStep = function() {
+    return step
+  };
 
   return {
-    setPalavraSecreta: setPalavraSecreta,
-    getEtapa: getEtapa,
-    getLacunas: getLacunas
+    setSecret: setSecret,
+    getStep: getStep,
+    getGap: getGap,
+    processInput: processInput
   };
 };
