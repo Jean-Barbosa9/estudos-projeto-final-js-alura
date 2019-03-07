bf020a720022db2edad2931f4f39a73913e74d27
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-03-07 14:07:58 -0300

    inclusao do arquivo que fara o controle da interaçao com o usuario

---
+++ js/controller.js
@@ -0,0 +1,4 @@
+/*
+  Interação com o usuário:
+  Ao digitar a palavra secreta e dar enter, as lacunas deverão aparecer e o placeholder do input deverá mudar para "chute". Além disso, a cada novo submit do form, agora deverá processar o input.
+*/
